4177a25fe3702cd08741a9092974659f884744dc
Neil Bartlett <njbartlett@gmail.com>
2017-12-22 15:41:53 +0000

    Add automatic test and update to make everything pass

---
+++ test-script.sh
@@ -0,0 +1,122 @@
+#!/usr/bin/env bash
+
+set -e
+
+ARCHETYPES_VERSION=0.0.3-SNAPSHOT
+PREFIX_DIR=target/test
+GROUP_ID=org.example
+PARENT_ARTIFACT_ID=${GROUP_ID}.parent
+MODULE_ARTIFACT_ID=${GROUP_ID}.module
+TEST_MODULE_ARTIFACT_ID=${MODULE_ARTIFACT_ID}.test
+STANDALONE_MODULE_ARTIFACT_ID=${GROUP_ID}.standalone
+VERSION=1.0-SNAPSHOT
+
+function runtests() {
+	echo STARTING $1 TEMPLATE CHECKS
+	echo ===========================
+	ORIGINAL_DIR=$(pwd)
+
+	# Clean and recreate test folder
+	rm -rf ${PREFIX_DIR}_${1}
+	mkdir -p ${PREFIX_DIR}_${1}
+	cd ${PREFIX_DIR}_${1}
+
+	echo ---  Generating top-level project ${PARENT_ARTIFACT_ID}
+	mvn -q -B archetype:generate \
+		-DarchetypeGroupId=com.effectiveosgi \
+		-DarchetypeVersion=0.0.3-SNAPSHOT \
+		-DarchetypeArtifactId=eosgi-project-archetype \
+		-DgroupId=${GROUP_ID} \
+		-DartifactId=${PARENT_ARTIFACT_ID} \
+		-Dversion=${VERSION} \
+		-DosgiLevel=${1}
+	cd ${PARENT_ARTIFACT_ID}
+	echo ---  Building top-level project...
+	mvn -q clean verify
+
+	# Generate module
+	echo ---  Generating child module ${MODULE_ARTIFACT_ID}
+	mvn -q -B archetype:generate \
+		-DarchetypeGroupId=com.effectiveosgi \
+		-DarchetypeVersion=0.0.3-SNAPSHOT \
+		-DarchetypeArtifactId=eosgi-module-archetype \
+		-DgroupId=${GROUP_ID} \
+		-DartifactId=${MODULE_ARTIFACT_ID} \
+		-Dversion=${VERSION} \
+		-DosgiLevel=${1}
+
+	# Insert new module into _index/pom.xml
+	sed -i .bak 's|<!-- Workspace Dependencies -->|&\
+		<dependency>\
+		<groupId>'${GROUP_ID}'</groupId>\
+		<artifactId>'${MODULE_ARTIFACT_ID}'</artifactId>\
+		<version>'${VERSION}'</version>\
+		</dependency>\
+	|' _index/pom.xml
+
+	# Insert new module to _assembly/application.bndrun
+	sed -i .bak 's|-runrequires: |&bnd.identity;id='${MODULE_ARTIFACT_ID}', |' _assembly/application.bndrun
+
+	# Build before resolving, this should fail
+	echo ---  Building project with child module '(pre-resolve)'
+	if mvn -q clean verify > /dev/null; then
+		echo ERROR: Build should NOT have succeeded before resolve
+		exit 1
+	fi
+
+	# Resolve and rebuild
+	echo ---  Resolving _assembly/application.bndrun
+	java -jar $2 resolve resolve -W _assembly/application.bndrun
+	echo ---  Building project with child module '(post-resolve)'
+	mvn -q clean verify
+
+	# Generate test module
+	echo ---  Generating child test module ${TEST_MODULE_ARTIFACT_ID}
+	mvn -q -B archetype:generate \
+		-DarchetypeGroupId=com.effectiveosgi \
+		-DarchetypeVersion=0.0.3-SNAPSHOT \
+		-DarchetypeArtifactId=eosgi-test-module-archetype \
+		-DgroupId=${GROUP_ID} \
+		-DartifactId=${TEST_MODULE_ARTIFACT_ID} \
+		-Dversion=${VERSION} \
+		-DosgiLevel=${1}
+
+	# Build with test module
+	echo ---  Building project with test module
+	mvn -q clean verify
+
+	# Generate standalone module
+	echo ---  Generating standalone module ${STANDALONE_MODULE_ARTIFACT_ID}
+	cd ..
+	mvn -q -B archetype:generate \
+		-DarchetypeGroupId=com.effectiveosgi \
+		-DarchetypeVersion=0.0.3-SNAPSHOT \
+		-DarchetypeArtifactId=eosgi-module-archetype \
+		-DgroupId=${GROUP_ID} \
+		-DartifactId=${STANDALONE_MODULE_ARTIFACT_ID} \
+		-Dversion=${VERSION} \
+		-DosgiLevel=${1}
+
+	# Build standalone module
+	cd ${STANDALONE_MODULE_ARTIFACT_ID}
+	mvn -q clean verify
+
+	cd $ORIGINAL_DIR
+}
+
+mkdir -p target
+BND3_PATH=$(pwd)/target/bnd3.jar
+BND4_PATH=$(pwd)/target/bnd4.jar
+
+echo Downloading bnd 3.5.0
+curl -o $BND3_PATH http://search.maven.org/remotecontent?filepath=biz/aQute/bnd/biz.aQute.bnd/3.5.0/biz.aQute.bnd-3.5.0.jar
+
+echo Downloading bnd 4.0.0-SNAPSHOT
+curl -o $BND4_PATH https://bndtools.ci.cloudbees.com/job/bnd.master/lastSuccessfulBuild/artifact/biz.aQute.bnd/generated/biz.aQute.bnd.jar
+
+runtests R6 $BND3_PATH
+runtests R7 $BND4_PATH
+
+echo =======
+echo SUCCESS
+echo
